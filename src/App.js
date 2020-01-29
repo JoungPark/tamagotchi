f7d0263658f216e8408af3c0dd86ca34364cd4e1
Joung Park <parkzh22@gmail.com>
2020-01-29 20:16:54 +1000

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Play from './Container/Play';
+import Play from './containers/Play';
 
 function App() {
   return (
